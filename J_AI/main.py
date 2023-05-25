a81737ae267c227f7d25514cc3e5b696c6d3c147
Bahniman <bahniman.rkm@gmail.com>
2023-05-25 15:52:37 +0530

    any meassage

---
+++ J_AI/main.py
@@ -0,0 +1,35 @@
+import speech_recognition as sr
+import webbrowser
+import win32com.client
+
+speaker = win32com.client.Dispatch("SAPI.SpVoice")
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.pause_threshold = 1
+        audio = r.listen(source)
+        try:
+            query = r.recognize_google(audio, language="en-in")
+            print(f"User said {query}")
+            return query
+        except Exception as e:
+            return "Some Error Occurred"
+
+
+if __name__ == '__main__':
+    speaker.Speak("Hello I am Raaahee")
+    while True:
+        print("Listening...")
+        query = takeCommand()
+        # todo: Add more sites
+        sites = [["youtube", "https://www.youtube.com"], ["wikipedia", "https://www.wikipedia.com"],
+                 ["google", "https://www.google.com"], ]
+        for site in sites:
+            if f"Open {site[0]}".lower() in query.lower():
+                # say()
+                speaker.Speak(f"Opening {site[0]} sir...")
+                webbrowser.open(site[1])
+
+        # speaker.Speak(text)
+
